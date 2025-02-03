611aea7605f817c87f964f6151028ef312c7c444
cntrvsy <benintangana@gmail.com>
2025-02-04 01:08:34 +0300

    todo admin panel, learning templates

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class TodoItems(models.Model):
+    item = models.CharField(max_length=200)
+    done = models.BooleanField(default=False)
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, HttpResponse
-
+from .models import TodoItems
 # Create your views here.
 
 def home(request):
-    # return render(request, 'home.html')
-    return HttpResponse("Hello World")
+    return render(request, 'home.html')
+    
+def todos(request):
+    items = TodoItems.objects.all()
+    return render(request, 'todos.html',{"todos": items})
